574160a5fd3c9c1f03235a59e5bf3bc3fe964e1f
jaegukhyun <jaeguk.hyun@intel.com>
2023-08-11 14:56:13 +0900

    Add class incremental configuration mixin

--- src/otx/algorithms/classification/adapters/mmcls/configurer.py
+++ src/otx/algorithms/classification/adapters/mmcls/configurer.py
@@ -14,6 +14,7 @@ from otx.algorithms.classification.adapters.mmcls.utils import (
     patch_datasets,
     patch_evaluation,
 )
+from otx.algorithms.common.adapters.mmcv.clsincr_mixin import IncrConfigurerMixin
 from otx.algorithms.common.adapters.mmcv.configurer import BaseConfigurer
 from otx.algorithms.common.adapters.mmcv.semisl_mixin import SemiSLConfigurerMixin
 from otx.algorithms.common.adapters.mmcv.utils.config_utils import (
@@ -205,7 +206,7 @@ CLASS_INC_DATASET = [
 WEIGHT_MIX_CLASSIFIER = ["CustomImageClassifier"]
 
 
-class IncrClassificationConfigurer(ClassificationConfigurer):
+class IncrClassificationConfigurer(IncrConfigurerMixin, ClassificationConfigurer):
     """Patch config to support incremental learning for classification."""
 
     def configure_task(self, cfg):
@@ -213,57 +214,12 @@ class IncrClassificationConfigurer(ClassificationConfigurer):
         super().configure_task(cfg)
         if "task_adapt" in cfg and self.task_adapt_type == "mpa":
             self.configure_task_adapt_hook(cfg)
-
-    def configure_task_adapt_hook(self, cfg):
-        """Add TaskAdaptHook for sampler."""
-        train_data_cfg = self.get_data_cfg(cfg, "train")
-        if self.training:
-            if train_data_cfg.type not in CLASS_INC_DATASET:
-                logger.warning(f"Class Incremental Learning for {train_data_cfg.type} is not yet supported!")
-            if "new_classes" not in train_data_cfg:
-                logger.warning('"new_classes" should be defined for incremental learning w/ current model.')
-
-            if cfg.model.type in WEIGHT_MIX_CLASSIFIER:
-                cfg.model.task_adapt = ConfigDict(
-                    src_classes=self.org_model_classes,
-                    dst_classes=self.model_classes,
-                )
-            else:
-                logger.warning(f"Weight mixing for {cfg.model.type} is not yet supported!")
-
-            train_data_cfg.classes = self.model_classes
-
-        if not cfg.model.get("multilabel", False) and not cfg.model.get("hierarchical", False):
-            efficient_mode = cfg["task_adapt"].get("efficient_mode", True)
-            sampler_type = "balanced"
-            self.configure_loss(cfg)
-        else:
-            efficient_mode = cfg["task_adapt"].get("efficient_mode", False)
-            sampler_type = "cls_incr"
-
-        if len(set(self.org_model_classes) & set(self.model_classes)) == 0 or set(self.org_model_classes) == set(
-            self.model_classes
-        ):
-            sampler_flag = False
-        else:
-            sampler_flag = True
-        # Update Task Adapt Hook
-        task_adapt_hook = ConfigDict(
-            type="TaskAdaptHook",
-            src_classes=self.org_model_classes,
-            dst_classes=self.model_classes,
-            model_type=cfg.model.type,
-            sampler_flag=sampler_flag,
-            sampler_type=sampler_type,
-            efficient_mode=efficient_mode,
-        )
-        update_or_add_custom_hook(cfg, task_adapt_hook)
+            if not cfg.model.get("multilabel", False) and not cfg.model.get("hierarchical", False):
+                self.configure_loss(cfg)
 
     def configure_loss(self, cfg):
         """Patch classification loss."""
-        if len(set(self.org_model_classes) & set(self.model_classes)) == 0 or set(self.org_model_classes) == set(
-            self.model_classes
-        ):
+        if not self.is_incremental():
             cfg.model.head.loss = dict(type="CrossEntropyLoss", loss_weight=1.0)
         else:
             cfg.model.head.loss = ConfigDict(
@@ -275,6 +231,17 @@ class IncrClassificationConfigurer(ClassificationConfigurer):
                 dst_classes=self.model_classes,
             )
             update_or_add_custom_hook(cfg, ib_loss_hook)
+
+    @staticmethod
+    def get_sampler_type(cfg):
+        """Return sampler type."""
+        if not cfg.model.get("multilabel", False) and not cfg.model.get("hierarchical", False):
+            cfg["task_adapt"].get("efficient_mode", True)
+            sampler_type = "balanced"
+        else:
+            cfg["task_adapt"].get("efficient_mode", False)
+            sampler_type = "cls_incr"
+        return sampler_type
 
 
 class SemiSLClassificationConfigurer(SemiSLConfigurerMixin, ClassificationConfigurer):
